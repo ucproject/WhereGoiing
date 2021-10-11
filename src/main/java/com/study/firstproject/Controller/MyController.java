2e449c95ad89334a77524276494a2505fb8c8687
ucproject <uclesson01@gmail.com>
2021-10-11 15:14:32 +0900

    new commit

---
+++ src/main/java/com/study/firstproject/Controller/MyController.java
@@ -0,0 +1,18 @@
+package com.study.firstproject.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * MyController
+ */
+@Controller
+public class MyController {
+
+    @GetMapping("/")
+    public String nicetoMeet(Model model){
+        model.addAttribute("username", "홍길동");
+        return "greetings";
+    }
+}
